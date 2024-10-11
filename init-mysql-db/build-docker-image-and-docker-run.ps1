fab6a2f02d7926f89d25504b8d9f2d3538e91dcd
mark.ku <mark.ku@ibuypower.com>
2024-10-11 11:01:54 +0800

    add mysql dockerfile and init scripts

---
+++ init-mysql-db/build-docker-image-and-docker-run.ps1
@@ -0,0 +1,9 @@
+$dockerfile = './Dockerfile'
+$imageName = "oauth-db:latest"
+$containerName="oauth-db"
+$port="33066:3306"
+
+docker build -t $imageName . -f $dockerfile
+docker run -d --name $containerName --restart=always -p $port $imageName 
+PAUSE
+

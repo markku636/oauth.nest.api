d4988de08ccd108d71e829519350704d2f8242fa
mark.ku <mark.ku@ibuypower.com>
2024-10-11 11:14:17 +0800

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('Oauth api')
+        .setDescription('The cats API description')
+        .setVersion('1.0')
+        .addTag('hello world!')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('doc', app, document);
 
     app.setViewEngine('ejs');
     app.setBaseViewsDir(join(__dirname, '..', '/src/views'));

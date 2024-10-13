5b3ba658826c1de11f7158702bb885843584942b
mark.ku <mark.ku@ibuypower.com>
2024-10-13 22:13:37 +0800

    update mock data scripts

--- init-mysql-db/data.sql
+++ init-mysql-db/data.sql
@@ -1,8 +1,8 @@
--- Insert 5 random users
+-- Insert 5 users with predefined email addresses
 INSERT INTO `User` (email, password, clientId, clientSecret)
 VALUES 
-('user1', 'password123', 'clientId1', 'clientSecret1'),
-('user2', 'password456', 'clientId2', 'clientSecret2'),
-('user3', 'password789', 'clientId3', 'clientSecret3'),
-('user4', 'passwordABC', 'clientId4', 'clientSecret4'),
-('user5', 'passwordXYZ', 'clientId5', 'clientSecret5');
+('user1@example.com', 'password123', 'clientId1', 'clientSecret1'),
+('user2@example.com', 'password456', 'clientId2', 'clientSecret2'),
+('user3@example.com', 'password789', 'clientId3', 'clientSecret3'),
+('user4@example.com', 'passwordABC', 'clientId4', 'clientSecret4'),
+('user5@example.com', 'passwordXYZ', 'clientId5', 'clientSecret5');
